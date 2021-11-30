3f25c14adaed35e502ce2f716145340d9ae15401
ricky9011 <rpchubukova@gmail.com>
2021-12-01 01:06:42 +0300

    Inicial

---
+++ src/main/java/KTD_Blog/KtdSpringApplication.java
@@ -0,0 +1,13 @@
+package KTD_Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtdSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtdSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KTD_Blog/KtdSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package KTD_Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtdSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
